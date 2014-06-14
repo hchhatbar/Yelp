0019c7d2f2ab34f4d7324a0cde2379377cafe622
hchhatbar <BH_MAC@Hemen-Chhatbars-MacBook-Pro.local>
2014-06-14 15:58:23 -0700

    Yelp Project set up

---
+++ Yelp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yelp
+//
+//  Created by Hemen Chhatbar on 6/14/14.
+//  Copyright (c) 2014 Hemen Chhatbar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
